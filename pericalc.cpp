2cc445dbd68575f708ed4ac806e0c79e24dfb6e5
aaardev <100081568+aaardev@users.noreply.github.com>
2022-03-06 11:52:42 +0530

    Perimeter calculator
    Calculates the perimeter of a square, modified area calculator!

---
+++ pericalc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float l;
+  float b;
+  cout << "Length\n";
+  cin >> l;
+  cout << "Breadth\n";
+  cin >> b;
+  float ans1 = l*2;
+  float ans2 = b*2;
+  float ans = l*b;
+  cout << ans<<" is the perimeter\n";
+} 
