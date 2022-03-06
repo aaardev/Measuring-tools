8e4bdb82f2a6755e0de553b706f7f2ffe88d357c
aaardev <100081568+aaardev@users.noreply.github.com>
2022-03-06 11:46:36 +0530

    Area Calculator
    It calculates the area of a 2D, four-sided shape.

---
+++ areacalc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float l;
+  float b;
+  cout << "Length\n";
+  cin >> l;
+  cout << "Breadth\n";
+  cin >> b;
+  float ans = l*b;
+  cout << ans<<" is the area\n";
+} 
